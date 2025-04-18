4293c69dc3137962c8c52c6536366fb687d72906
amaechijude <ugwuamaechi5@gmail.com>
2025-04-18 21:49:06 +0100

    footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,17 @@
 import AboutMe from "@/components/AboutMe";
 import Contact from "@/components/Contact";
+import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import Projects from "@/components/Project";
-import { Section } from "lucide-react";
-
 
 export default function Home() {
-
   return (
     <>
       <Hero />
       <Projects />
       <AboutMe />
-      <Section id="contact">
-        <Contact />
-      </Section>
+      <Contact />
+      <Footer />
     </>
   );
 }
