3a66ffdf05ce0a4a6903d9eac9e2ecd5b6718737
amaechijude <ugwuamaechi5@gmail.com>
2025-04-19 14:10:26 +0100

    data

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,12 +1,36 @@
 export const projectsData = [
   {
-    title: "Project Alpha",
+    title: "AspNetCoreEcommerce",
+    description: "A robust ecommerce api built with .NET",
+    tags: ["ASP.NET Core", "C#", ".NET", "PostgreSQL", "Azure"],
+    imageUrl: "/projects/alpha.png", // Replace with actual image path/URL
+    liveUrl: null, // No live demo for this API
+    repoUrl: "https://github.com/amaechijude/aspnet-ecommerce", // Replace with GitHub repo URL
+  },
+  {
+    title: "TailorConnect",
+    description: "A fashion platform that connects designers with customers.",
+    tags: ["Django", "Bootstrap", "Azure", "PostgreSQL", "Celey", ],
+    imageUrl: "/projects/alpha.png", // Replace with actual image path/URL
+    liveUrl: "https://tailorconnect.store",
+    repoUrl: "https://github.com/amaechijude/tailorconnect", // Replace with GitHub repo URL
+  },
+  {
+    title: "Client Portal SaaS",
+    description: "A multi-tenant project management tool built with .NET and Clean Architecture.",
+    tags: [".NET", "Next.js", "PostgreSQL", "Serilog"],
+    imageUrl: "/projects/alpha.png", // Replace with actual image path/URL
+    liveUrl: "#",
+    repoUrl: "https://github.com/amaechijude/client-portal", // Replace with GitHub repo URL"
+  },
+  {
+    title: "Inventory Tracker",
     description:
-      "A full-stack web application using Django Rest Framework and React.",
+      "A full-stack web application using Django Rest Framework, React and NextJs for inventory management.",
     tags: ["Django", "React", "Python", "Docker", "PostgreSQL"],
     imageUrl: "/projects/alpha.png",
     liveUrl: "#", // Replace with live demo URL
-    repoUrl: "#", // Replace with GitHub repo URL
+    repoUrl: "https://github.com/amaechijude/inventory-tracker", // Replace with GitHub repo URL
   },
   {
     title: "Service Hub API",
@@ -58,6 +82,7 @@ export const experienceData = [
 export const skillsData = [
   "Python",
   "C#",
+  ".NET",
   "JavaScript",
   "TypeScript",
   "Django",
